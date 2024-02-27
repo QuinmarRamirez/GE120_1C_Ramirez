3b8a88017c5c7ca40bc5ec70aa14d9fe4176fb4d
Quinmar Ramirez <qqramirez@up.edu.ph>
2024-02-27 17:29:14 +0800

    Submit Exercise 1

--- Exercise1/Exercise1_Ramirez.py
+++ Exercise1/Exercise1_Ramirez.py
@@ -1,14 +1,14 @@
 """
 Hi, this is exercise 1. 
 I am Quinmar Ramirez
-From Maragondon, Cavite
+2023-14871
+BS Geodetic Engineering
 """
 
 dms = 118.42069
 
-#Getting degreee
+#Getting degree
 degree = int(dms)
-
 print("DEGREE:", degree)
 
 #Getting minutes
@@ -20,9 +20,10 @@ print("MINUTES", minutes)
 seconds = (minutes - minutes_fractional) * 60
 print("SECONDS", seconds)
 
+#The Degree-Minute-Second 
 print("DMS: "+ str(degree) + "-" +str(minutes_fractional) + "-" + str(round(seconds, 2)))
 
-# String input
+#Converting DMS to DD
 dms = "118-25-14.48"
 values = dms.split("-")
 
@@ -32,6 +33,7 @@ seconds = float(values[2])
 
 dd= degrees + (minutes/60) +(seconds/3600)
 
-print("VALUE:", round(dd,6))
+#The Decimal Degree
+print("DD:", round(dd,6))
 
 

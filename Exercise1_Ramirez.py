930494bec5d7f4504ef62cfcc4336bee5d07023b
Quinmar Ramirez <qqramirez@up.edu.ph>
2024-02-27 16:17:06 +0800

    Exercsie 1

---
+++ Exercise1_Ramirez.py
@@ -0,0 +1,3 @@
+"""
+Hi, this is exercise 1. 
+I am Quinmar Ramirez
---
+++ Lecture2_Ramirez.py
@@ -0,0 +1,29 @@
+print("Kumusta Mundo")
+print(type("Kumusta Mundo"))
+print(type(str(1)))
+print(type("STRING")) #str
+print(type(54)) #int
+print(type(3.1415265358)) #float
+print(type(1+4)) #complex
+
+
+print(type(float(54))) #convert int to float
+
+x=2
+y=13
+
+print("Sum of of X and Y: ", x + y) #addition
+print("Difference of X and Y: ", x - y) #subtraction
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** y)
+print(x % y)
+print(abs(y))
+print(float(y))
+print(complex(y))
+print(divmod(x // y))
+
+
+dog = ("golden retriever", "husky", "aspin", "poodle")
+print(dog(0))

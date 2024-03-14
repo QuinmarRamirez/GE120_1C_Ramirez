d4f12d9aa3826d5117f3192a02767840f01d46e9
Quinmar Ramirez <qqramirez@up.edu.ph>
2024-03-14 23:35:54 +0800

    Added Notes for Lecture 3

--- Lecture3_Ramirez.py
+++ Lecture3_Ramirez.py
@@ -50,42 +50,4 @@
 # print("PASOK NA REC")
 # print("-----END------")
 
-# ASSIGNING AZIMUTH TO BEARING
-    if azimuth > 0 and azimuth < 90:
-        bearing = 'S {: ^10} W' .format(azimuth)
-    elif azimuth > 90 and azimuth < 180:
-        bearing = 'N {: ^10} W' .format(180 - azimuth)
-    elif azimuth > 180 and azimuth < 270:
-        bearing = 'N {: ^10} E' .format(azimuth -180)
-    elif azimuth > 270 and azimuth < 360:
-        bearing = 'S {: ^10} E' .format(360 - azimuth)
-    elif azimuth ==0:
-        bearing = "DUE SOUTH"
-    elif azimuth ==90:
-        bearing = "DUE WEST"
-    elif azimuth ==180:
-        bearing = "DUE NORTH"
-    elif azimuth ==270:
-        bearing = "DUE EAST"
-    else:
-        bearing = "NOT APPLICABLE"
-
-    line = (counter, distance, bearing)  # create tuple of the line
-    lines.append(line)
-
-    # Ask for input
-    yn = input("DO YOU WANT TO ADD A NEW LINE? [yes/no]  ")
-    if yn.lower() =="yes" or yn.lower() == "y":
-        counter = counter + 1
-        continue
-    else:
-        break
-
-print ("\n\n")
-print ('{: ^10} {: ^10} {: ^10}' .format ("LINE NO.", "DISTANCE", "BEARING"))
-for line in lines:
-     print ('{: ^10} {: ^10} {: ^10}' .format (line [0], line [1], line [2]))
-
-print ("-----END-----")
-
 
